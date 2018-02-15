a763a532b3f6a6bcfb1132570838726eb49a2d26
PReiZ <36497489+PReiZ@users.noreply.github.com>
2018-02-15 15:01:58 +0900

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const botSettings = require("./botSettings.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log('bot is ready! ${bot.user.username}');
+
+  try{
+    let link = await bot.GeneralInvite(["ADMINISTRATOR"]);
+    console.log(link);
+  } catch(e) {
+    console.log(e.stack);
+  }
+});
+
+bot.in("message", async message => {
+  if(message.author.bot) return;
+  if(message.)
+});
+
+bot.login(botSettings.token);
